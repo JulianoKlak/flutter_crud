2c1b0d81e3c0e601fc9f8a760acae88890cc7eec
JulianoKlak <julianoklak@gmail.com>
2025-09-03 23:29:37 -0300

    criando classe usuários e dados fixos

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,28 @@
+import '../models/user.dart';
+
+const DUMMY_USERS = {
+    '1': const User(
+    "id": 1,
+    "name": "Alice Johnson",
+    "email": "alice@coldmail.com",
+    "avatarUrl": "https://cdn.pixabay.com/photo/2013/07/13/12/06/woman-159169_960_720.png"
+    ),
+    '2': const User(
+    "id": 2,
+    "name": "Bob Smith",
+    "email": "bob@coldmail.com",
+    "avatarUrl": "https://cdn.pixabay.com/photo/2016/11/18/23/38/child-1837375_1280.png"
+    ),
+    '3': const User(
+    "id": 3,
+    "name": "Charlie Brown",
+    "email": "charlie@peanuts.com",
+    "avatarUrl": "https://cdn.pixabay.com/photo/2021/03/26/09/32/snoopy-6125374_1280.jpg"
+    ),
+    '4': const User(
+    "id": 4,
+    "name": "Diana Prince",
+    "email": "diana@royal.com",
+    "avatarUrl": "https://cdn.pixabay.com/photo/2022/07/15/09/48/queen-7322821_1280.png"
+    ),
+};
